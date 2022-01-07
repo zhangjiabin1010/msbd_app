8b03cb42c8423968da45019a6661b65c047e306f
zhangjiabin1010 <zhangjiabin1010@gmail.com>
2022-01-07 17:28:13 +0800

    searchbar

--- lib/pages/learn/finished.dart
+++ lib/pages/learn/finished.dart
@@ -15,10 +15,78 @@ class _FinishedListState extends State<FinishedList> {
         IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
-              print('开始搜索');
+              showSearch(context: context, delegate: searchBarDelegate());
             }),
       ]),
       body: Column(children: []),
     );
+  }
+}
+
+class searchBarDelegate extends SearchDelegate<String> {
+//重写右侧的图标
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        //将搜索内容置为空
+        onPressed: () => query = "",
+      )
+    ];
+  }
+
+//重写返回图标
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        //关闭上下文，当前页面
+        onPressed: () => close(context, ""));
+  }
+
+  //重写搜索结果
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      child: Card(
+        color: Colors.redAccent,
+        child: Center(
+          // child: Text(query),
+          child: Text("ssssssssssssssssssssss"),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    const recentSuggest = ["wangcai推荐-1", "nvfengsi推荐-2"];
+    const searchList = ["wangcai", "xiaoxianrou", "dachangtui", "nvfengsi"];
+
+    final suggestionList = query.isEmpty
+        ? recentSuggest
+        : searchList.where((input) => input.startsWith(query)).toList();
+
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) => ListTile(
+              title: RichText(
+                text: TextSpan(
+                    text: suggestionList[index].substring(0, query.length),
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: suggestionList[index].substring(query.length),
+                          style: TextStyle(color: Colors.grey))
+                    ]),
+              ),
+            ));
   }
 }
